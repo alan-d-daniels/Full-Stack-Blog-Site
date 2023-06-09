82a379e303e43d31682919e95923a2212ef4ee6b
alan-d-daniels <dalan@karunya.edu.in>
2023-06-09 11:01:19 +0530

    update code in github

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const mongoose= require("mongoose");
 const encrypt= require("mongoose-encryption");
 
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -44,6 +41,12 @@ const userSchema= new mongoose.Schema({
   
 });
 
+const aboutSchema= new mongoose.Schema({
+  name:String,
+  email:String,  
+  message:String  
+});
+
 //encrypting the password
 
 const secret="Thisisourlittlesecret."
@@ -55,6 +58,9 @@ const Post= mongoose.model("Post",postSchema);
 
 //model for the user details
 const User = mongoose.model("User",userSchema);
+
+//model for the user details
+const About = mongoose.model("About",aboutSchema);
 
 
 
@@ -84,9 +90,7 @@ app.post("/",(req,res)=>{
   if(req.body.submit_param==="submit_value"){
     res.render("home")
   }
-  if(req.body.contact==="details"){
-    res.render("feedback")
-  }
+  
 
   if(req.body.username!='' && req.body.password!=''){
     const email=req.body.username;
@@ -114,12 +118,9 @@ app.post("/",(req,res)=>{
         res.render("error");
       }
   })
-
   }
-
   else{
     res.render("error");
-
   }
 });
 
@@ -133,46 +134,62 @@ app.get("/register",(req,res)=>{
 app.post("/register",(req,res)=>{
 
   if(req.body.username!='' && req.body.password!='' ){
-    const newUser= new User({
-      email:req.body.username,
-      password:req.body.password
-    });
-  
-    newUser.save((err)=>{
+    User.findOne({email:req.body.username},(err,foundUser)=>{
       if(err)
         console.log("Error");
-      else  
-      Post.find((err,posts)=>{
-        res.redirect("/");    
-      }); 
-    })
-
+      else
+        if(foundUser){
+          res.render("existing")          
+        }       
+  
+        else{
+          const newUser= new User({
+            email:req.body.username,
+            password:req.body.password
+          });
+        
+          newUser.save((err)=>{
+            if(err)
+              console.log("Error");
+            else  
+            Post.find((err,posts)=>{
+              res.redirect("/");    
+            }); 
+          })
+        }
+    });
   }
-
   else{
-    res.render("error");
-    
-  } 
-
+    res.render("error");    
+  }
 });
 
 // About page GET
 
 app.get("/about",(req,res)=>{
-  res.render("about",{
-    aboutContent: aboutContent
-  })
+  res.render("about")
 });
 
 
 //Contact page GET
 
 app.get("/contact",(req,res)=>{  
-  res.render("contact",{
-    contactContent: contactContent
-  });
+  res.render("contact");
 });
 
+app.post("/contact",(req,res)=>{ 
+  console.log(req.body.message);
+  const about= new About({
+    name:req.body.name,
+    email: req.body.email,    
+    message: req.body.message
+  }); 
+  about.save((err)=>{
+    if(!err)
+    res.render("feedback");
+  });
+  
+});
 
 //Compose page GET
 
@@ -182,7 +199,9 @@ app.get("/compose",(req,res)=>{
 
 // Compose page POST
 
-app.post("/compose",(req,res)=>{  
+app.post("/compose",(req,res)=>{     
+
+  
 
   const post= new Post({
     genreName: _.kebabCase(req.body.genreName),
